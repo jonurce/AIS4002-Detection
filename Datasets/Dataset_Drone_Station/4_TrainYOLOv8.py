6ca7c8d9b4838592259eb622f3bdcc46dd79ce5f
jonurce <jonur@stud.ntnu.no>
2025-04-17 16:16:32 +0200

    Training and improving yolo

--- Datasets/Dataset_Drone_Station/4_TrainYOLOv8.py
+++ Datasets/Dataset_Drone_Station/4_TrainYOLOv8.py
@@ -1,21 +1,28 @@
 from ultralytics import YOLO
 
 # Load a pre-trained YOLOv8 model (nano for speed, or medium/large for accuracy)
-model = YOLO("yolov8n.pt")  # Options: yolov8n.pt, yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
+model = YOLO("yolov8l.pt")  # Options: yolov8n.pt, yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
 
 # Train the model
 results = model.train(
     data="data_drone_station.yaml",  # Path to dataset config
-    epochs=50,         # Number of training epochs
+    epochs=60,         # Number of training epochs
     imgsz=640,         # Image size (resize to 640x640)
     batch=8,          # Batch size (adjust based on GPU memory)
     workers = 4,        # CPU cores
-    device=-1,          # Use GPU (set to -1 for CPU)
-    name="yolo_drone_station",   # Experiment name
+    device="cpu",          # Use GPU (set to -1 for CPU)
+    name="yolov8l_drone_station",   # Experiment name
     project="Runs_Drone_Station",
-    patience=10,       # Early stopping after 10 epochs with no improvement
+    patience=0,       # Early stopping after 10 epochs with no improvement
     save=True,         # Save checkpoints
-    save_period=10     # Save checkpoint every 10 epochs
+    save_period=10,     # Save checkpoint every 10 epochs
+# Data augmentation
+    hsv_h=0.015,                     # Hue augmentation
+    hsv_s=0.7,                       # Saturation augmentation
+    hsv_v=0.4,                       # Value (brightness) augmentation
+    rotate=30,                       # Rotation up to ±10 degrees
+    translate=0.2,                    # Translation up to 10% of image
+    scale=0.5                        # Scaling up to 50%
 )
 
 # Print training results
--- Datasets/Dataset_Drone_Station/5_TestYOLOv8.py
+++ Datasets/Dataset_Drone_Station/5_TestYOLOv8.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import os
 
 # Load the trained model
-model = YOLO("runs/detect/yolo_a_b/weights/best.pt")
+model = YOLO("Runs_Drone_Station/yolo_drone_station/weights/best.pt")
 
 # Test on test set
 results = model.predict(
@@ -11,7 +11,8 @@ results = model.predict(
     iou=0.5,   # IoU threshold for NMS
     save=True, # Save predictions
     save_txt=True,  # Save YOLO-format predictions
-    name="YOLOv8_test_predictions"  # Output folder
+    name="YOLOv8_test_predictions",  # Output folder
+    project="Tests_Drone_Station"
 )
 
 # Print results
